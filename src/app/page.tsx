3750955d95b80078087a503fd4048833b4e5f7f1
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-01-03 10:01:13 -0300

    feat: create div main separator component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Card } from "@/components/base/card";
+import { AboutMe } from "@/components/base/AboutMe";
+import { Card } from "@/components/base/Card";
 import Image from "next/image";
 
 
@@ -6,9 +7,7 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<div className="bg-[#EBF3FF] w-screen h-screen flex justify-center items-center gap-[10px]">
-			<div className="flex flex-col bg-[#002352] w-[400px] rounded p-[20px]">
-				<Card/>
-			</div>
+			<AboutMe/>
 			<div className="flex bg-[#EBF2FF] w-[900px] h-[400px] shadow-md rounded">
 
 			</div>
---
+++ src/components/base/AboutMe/index.tsx
@@ -0,0 +1,12 @@
+import { Card } from "../Card";
+import { SeparatorMain } from "../Separator";
+
+export function AboutMe() {
+    return (
+        <div className="flex flex-col bg-[#002352] w-[400px] rounded p-[20px] gap-[10px]">
+            <Card />
+            <SeparatorMain/>
+            
+        </div>
+    );
+}
