add15d42de5e4c9286e31b7a519added9a65254e
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-01-04 19:51:24 -0300

    fix: adjust footer width for larger screen

--- src/components/base/Footer/index.tsx
+++ src/components/base/Footer/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <div className="flex xl:flex-col bg-blue900 xl:h-[773px] gap-2 shadow-md rounded">
-            <div className="flex xl:flex-col xl:h-[100%] h-[70px]  w-[100%] justify-center items-center gap-10
+            <div className="flex xl:flex-col xl:h-[100%] h-[70px] xl:w-[70px] w-[100%] justify-center items-center gap-10
             ">
                 <Briefcase size={30} color="#f5efef" weight="bold" />
                 <Link href="/skills">
