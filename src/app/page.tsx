020c4400d998e23f11b6844a1b536d20a7dd27bc
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-01-04 10:40:00 -0300

    feat: create title component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import { Hero } from "@/components/base/Hero";
+
 export default function Home() {
 	return (
 		<>
-			<div className="flex bg-blue100 w-[900px] h-[773px] shadow-md rounded p-[20px]">
-
-			</div>
+			<Hero/>
 			<div className="flex bg-blue900 w-[50px] h-[773px] shadow-md rounded">
 
 			</div>
---
+++ src/components/base/Hero/index.tsx
@@ -0,0 +1,9 @@
+import { Title } from "../Title";
+
+export function Hero () {
+    return (
+        <div className="flex gap-[20px] bg-blue100 w-[900px] h-[773px] shadow-md rounded p-[20px]">
+            <Title variant="Sobre mim"/>
+        </div>
+    );
+}
