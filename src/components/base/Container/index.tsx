a94a388716374ceb88840c583f2b58c9a0f1708a
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-01-05 19:56:28 -0300

    feat: implement Sidebar component and refactor Container to use it

--- src/components/base/Container/index.tsx
+++ src/components/base/Container/index.tsx
@@ -1,14 +1,11 @@
 'use client'
 
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Separator } from "@/components/ui/separator";
-import { cn } from "@/lib/utils";
 import { AddressBook, Briefcase, Code, House, List, X } from "@phosphor-icons/react/dist/ssr";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import { Navigation } from "../Navigation";
-import { MadeBy } from "./(component)/MadeBy";
+import { Sidebar } from "./(component)/Sidebar";
 
 type ContainerProps = {
     type?: "main" | "aside" | "footer"
@@ -16,10 +13,10 @@ type ContainerProps = {
 }
 
 export function Container({ type = "main", children }: ContainerProps) {
-    const [show, setShow] = useState("side-hidden");
+    const [show, setShow] = useState("show");
 
     function handleSideShow() {
-        show === "side-hidden" ? setShow("side-show") : setShow("side-hidden")
+        show === "hidden" ? setShow("show") : setShow("hidden")
     }
 
     return (
@@ -28,23 +25,7 @@ export function Container({ type = "main", children }: ContainerProps) {
                 <span onClick={handleSideShow} className="absolute md:top-5 top-3 right-5 bg-blue900 rounded p-1 cursor-pointer">
                     <List size={30} color="#f5efef" weight="light" />
                 </span>
-                <div className={cn("rounded-lg flex flex-col side bg-blue900 duration-500 p-5 text-blue100 gap-5 ", show)}>
-                    <span onClick={handleSideShow} className={cn("absolute right-7 top-7 hover:bg-blue100p20 duration-500 rounded cursor-pointer", show === "side-hidden" ? "hidden" : "block")}>
-                        <X size={30} color="#fafafa" weight="light" />
-                    </span>
-                    <div className="flex gap-3">
-                        <span><Image className="rounded-full" src="/repositories/Guilherme.webp" width={50} height={50} alt="profile picture" /></span>
-                        <div className="flex flex-col justify-center">
-                            <h1 className="text-sm text-blue100">Front-end Developer</h1>
-                            <p className="text-xs text-blue100p80">@Guilherme Oliveira</p>
-                        </div>
-                    </div>
-                    <Separator />
-                    <Navigation type="skill" />
-                    <Separator />
-                    <Navigation type="social" />
-                    <MadeBy/>
-                </div>
+                <Sidebar handleSideShow={handleSideShow} show={show}/>
                 {children}
             </div>
         </ScrollArea>
