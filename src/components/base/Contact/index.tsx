5dbf4e9087e1742675ca6e7b63d50cd090aadbc8
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-01-05 13:03:18 -0300

    style: add styles to contacts component

--- src/components/base/Contact/index.tsx
+++ src/components/base/Contact/index.tsx
@@ -1,16 +1,16 @@
 import { socialsConfig } from "@/config/socials.config";
 import Link from "next/link";
 
-export function Contact () {
+export function Contact() {
     const socials = socialsConfig;
 
     return (
         <>
-            {socials && 
-                socials.map(({id, title, href, Icon}) => (
-                    <li key={id} className="flex items-center gap-2 mb-[10px]">
-                        <Icon size={20}/>
-                        <Link href={href} title={title} rel="noopener noreferrer" target="_blank" className="text-sm">
+            {socials &&
+                socials.map(({ id, title, href, Icon }) => (
+                    <li key={id} className="mb-[10px]">
+                        <Link href={href} title={title} rel="noopener noreferrer" target="_blank" className="text-sm text-blue100 flex items-center gap-2 hover:gap-3 hover:underline duration-300">
+                            <Icon size={20} />
                             {title}
                         </Link>
                     </li>
