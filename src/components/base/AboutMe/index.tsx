171bd22d1702a7ec2f400da7a042dd139bd14608
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-01-05 20:06:32 -0300

    refactor: remove any imports

--- src/components/base/AboutMe/index.tsx
+++ src/components/base/AboutMe/index.tsx
@@ -1,11 +1,8 @@
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Semantic } from "../Semantic";
 import { Title } from "../Title";
 import { Hero } from "./(component)/hero";
 import { Repositories } from "./(component)/repositories";
 import { Technologies } from "./(component)/skills";
 import { Button } from "../Button";
-import { Container } from "../Container";
 
 export function AboutMe() {
     return (
